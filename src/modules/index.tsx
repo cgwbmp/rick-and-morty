6b450f9938760d8c0653df6363f7bc66912cd0bd
Alena <cgwbmp@gmail.com>
2019-09-24 00:01:54 +0500

    Add characters cards

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
 
+interface Character {
+  id: string,
+  name: string,
+}
+
+const characters: Array<Character> = [
+  { id: '1', name: 'Character 1' },
+  { id: '2', name: 'Character 2' },
+  { id: '3', name: 'Character 3' },
+  { id: '4', name: 'Character 4' },
+];
+
 const Home: React.FC = () => (
   <div>
     <Head>
@@ -9,6 +21,13 @@ const Home: React.FC = () => (
     <header>
       <h1>Welcome to The Rick and Morty Wiki!</h1>
     </header>
+    <section>
+      {characters.map((character: Character) => (
+        <div className="card" key={character.id}>
+          <h4>{character.name}</h4>
+        </div>
+      ))}
+    </section>
     <style jsx>
       {`
         header {
@@ -27,6 +46,24 @@ const Home: React.FC = () => (
           line-height: 1.25;
           font-size: 48px;
           text-align: center;
+        }
+        section {
+          display: flex;
+          flex-wrap: wrap;
+          margin 10px -10px;
+        }
+        .card {
+          width: 45%;
+          margin 10px;
+          padding: 20px;
+          background: #bbb;
+          color: #000;
+          box-sizing: border-box;
+        }
+        .card h4 {
+          font-family: monospace;
+          font-size: 18px;
+          margin: 0;
         }
       `}
     </style>
