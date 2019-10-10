382cd75877ab9f59e58d0be983c17db0ad362230
Alena <cgwbmp@gmail.com>
2019-10-10 22:08:26 +0500

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c2d017b9e50dd81e0222";
+/******/ 	var hotCurrentHash = "4d496a8fd047f30ff99e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
