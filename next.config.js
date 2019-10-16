cffc14cfa35a8377780fbc3271158410d2193783
Alena <cgwbmp@gmail.com>
2019-10-16 23:35:32 +0500

    Add offline support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 const withCSS = require('@zeit/next-css');
+const withOffline = require('next-offline');
 
-module.exports = withCSS({
+module.exports = withOffline(withCSS({
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:4]',
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/rick-and-morty' : '',
-});
+  assetPrefix: (process.env.NODE_ENV === 'production') ? '/rick-and-morty' : '',
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'CacheFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}));
